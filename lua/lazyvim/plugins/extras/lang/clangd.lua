9c3c11c03a2c92396657768f5290c6e9fd63e69f
Atif <iftakhar.awal@gmail.com>
2025-07-23 15:18:22 +0600

    fix(clangd): replace deprecated `find_git_ancestor`

--- lua/lazyvim/plugins/extras/lang/clangd.lua
+++ lua/lazyvim/plugins/extras/lang/clangd.lua
@@ -73,7 +73,7 @@ return {
               )(fname) or require("lspconfig.util").root_pattern(
                 "compile_commands.json",
                 "compile_flags.txt"
-              )(fname) or require("lspconfig.util").find_git_ancestor(fname)
+              )(fname) or vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
             end
             if type(bufnr) == "string" then
               return root_directory(bufnr)
